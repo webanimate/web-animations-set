fd8322a95cdf1bfba7ce06ad13f88687f8a1aed6
Web Animate <60752454+webanimate@users.noreply.github.com>
2020-12-01 02:27:40 +0200

    feat: check and fix animations with eslint and prettier

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,34 @@
 #!/usr/bin/env node
+
 const fs = require('fs')
 const path = require('path')
+const execa = require('execa')
+
+const onResponse = (response) => {
+  console.log(response)
+  // console.log(response.stdout)
+}
+
+const onError = (error) => {
+  console.log(error)
+  console.log(error.stdout)
+}
 
 fs.writeFileSync(path.join(process.cwd(), 'file.js'), `export default {\n\n}\n`)
+
+execa(path.join('.', 'node_modules', '.bin', 'eslint') + ' --fix "**/*.*" --resolve-plugins-relative-to . --ignore-path ' + path.join('.', 'node_modules', 'web-animations-set', 'src',  '.eslintignore') + ' --config ' + path.join('.', 'node_modules', 'web-animations-set', 'src', '.eslintrc.json')).then(
+  (response) => {
+    onResponse(response)
+    execa(path.join('.', 'node_modules', '.bin', 'prettier') + ' --write "**/*.*" -u --print-width 280 --no-semi --single-quote --ignore-path ' + path.join('.', 'node_modules', 'web-animations-set', 'src', '.prettierignore')).then(
+      (response) => {
+        onResponse(response)
+      },
+      (error) => {
+        onError(error)
+      }
+    )
+  },
+  (error) => {
+    onError(error)
+  }
+)
